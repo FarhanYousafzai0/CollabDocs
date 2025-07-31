e6a34e7fdab13e1f3623f5739ce383148e92dc5d
Farhan Yousafzai <coderwithferry@gmail.com>
2025-08-01 01:22:57 +0500

    Last

--- app/(root)/document/[id]/page.jsx
+++ app/(root)/document/[id]/page.jsx
@@ -1,11 +1,16 @@
+"use client"
+
+
 import { Editor } from '@/components/editor/Editor'
 import React from 'react'
 
 const Document = () => {
   return (
   <>
-  <Editor/>
-  
+
+
+<Editor/>
+
   </>
   )
 }
